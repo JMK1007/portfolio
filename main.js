ac346e2faa9699b984befce8a4e776008bfe5411
JMK1007 <67947887+JMK1007@users.noreply.github.com>
2020-08-05 19:33:00 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -22,7 +22,13 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 //Handle click on "contact me" button on home
